542480f094c3326cb8cecbb8223deed3d6158037
George DiNicola <george.j.dinicola@gmail.com>
2024-03-14 13:16:50 -0700

    added functionality for grouping state abbreviations and full proper names

--- scraper.go
+++ scraper.go
@@ -11,6 +11,28 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+var StateMap = map[string]string{
+	"AL": "ALABAMA", "AK": "ALASKA", "AZ": "ARIZONA", "AR": "ARKANSAS", "CA": "CALIFORNIA",
+	"CO": "COLORADO", "CT": "CONNECTICUT", "DE": "DELAWARE", "FL": "FLORIDA", "GA": "GEORGIA",
+	"HI": "HAWAII", "ID": "IDAHO", "IL": "ILLINOIS", "IN": "INDIANA", "IA": "IOWA",
+	"KS": "KANSAS", "KY": "KENTUCKY", "LA": "LOUISIANA", "ME": "MAINE", "MD": "MARYLAND",
+	"MA": "MASSACHUSETTS", "MI": "MICHIGAN", "MN": "MINNESOTA", "MS": "MISSISSIPPI", "MO": "MISSOURI",
+	"MT": "MONTANA", "NE": "NEBRASKA", "NV": "NEVADA", "NH": "NEW HAMPSHIRE", "NJ": "NEW JERSEY",
+	"NM": "NEW MEXICO", "NY": "NEW YORK", "NC": "NORTH CAROLINA", "ND": "NORTH DAKOTA", "OH": "OHIO",
+	"OK": "OKLAHOMA", "OR": "OREGON", "PA": "PENNSYLVANIA", "RI": "RHODE ISLAND", "SC": "SOUTH CAROLINA",
+	"SD": "SOUTH DAKOTA", "TN": "TENNESSEE", "TX": "TEXAS", "UT": "UTAH", "VT": "VERMONT",
+	"VA": "VIRGINIA", "WA": "WASHINGTON", "WV": "WEST VIRGINIA", "WI": "WISCONSIN", "WY": "WYOMING",
+}
+
+func getStateFullName(input string) string {
+	upInput := strings.ToUpper(input) // Convert input to uppercase to match the map keys
+	if fullName, ok := StateMap[upInput]; ok {
+		return fullName // Return the full name if found
+	} else {
+		return upInput
+	}
+}
+
 func getContestantInformation(doc *goquery.Document, episode string) ([]Contestant, error) {
 	var contestants []Contestant
 	query := fmt.Sprintf("table[aria-labelledby='%s-label'] .contestant", episode)
@@ -25,6 +47,7 @@ func getContestantInformation(doc *goquery.Document, episode string) ([]Contesta
 
 			if len(homeCityState) > 1 {
 				homeCity, homeState = strings.TrimSpace(homeCityState[0]), strings.TrimSpace(homeCityState[1])
+				homeState = getStateFullName(homeState)
 			}
 		}
 
